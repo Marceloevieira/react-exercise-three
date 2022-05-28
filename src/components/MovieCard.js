be998809f42798d3a7bb27d9d69ce18d0e5e7f19
Marcelo Evangelista Vieira <m.evangelista.vieira@gmail.com>
2022-05-28 13:15:08 -0400

    conforme orientação do Professor Samuel, alterado as navegaçoes em location para componentes especificios do react-router-dom

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -8,22 +8,22 @@ export const MovieCard = ({ movie: { id, title, release_date, poster_path } }) =
 
     //https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg   || w500 => size
     return (
-        <Card
-            key={id}
-            style={{ width: '18rem', cursor: 'pointer' }}
-            onClick={(e) => window.location = `/movie/${id}`}
-        >
-            <Card.Img  variant="top" src={`https://image.tmdb.org/t/p/original/${poster_path}`} />
-            <Card.Body>
-                <Card.Title
-                    style={{ cursor: 'pointer' }}
-                    onClick={(e) => window.location = `/movie/${id}`}
-                >{title}</Card.Title>
-                <Card.Text>
-                    {dateExtenso.toLocaleDateString('pt-BR', { year: 'numeric', month: 'long', day: 'numeric' })}
-                </Card.Text>
-            </Card.Body>
-        </Card>
+        <Link to={`/movie/${id}`} style={{textDecoration: 'inherit', color: 'inherit'}}>
+            <Card
+                key={id}
+                style={{ width: '18rem', cursor: 'pointer' }}
+            >
+                <Card.Img  variant="top" src={`https://image.tmdb.org/t/p/original/${poster_path}`} />
+                <Card.Body>
+                    <Card.Title
+                        style={{ cursor: 'pointer' }}
+                    >{title}</Card.Title>
+                    <Card.Text>
+                        {dateExtenso.toLocaleDateString('pt-BR', { year: 'numeric', month: 'long', day: 'numeric' })}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Link>
     );
 };
 
